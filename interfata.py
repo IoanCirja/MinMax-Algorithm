50f5620fab92c4a36971c6cafa463d4594a6ad9c
IoanCirja <ioan.cirja.00@gmail.com>
2024-12-12 11:38:14 +0200

    win msg

--- interfata.py
+++ interfata.py
@@ -18,7 +18,9 @@ width = COLOANE * DIMENSIUNEBULINA
 height = (RANDURI + 1) * DIMENSIUNEBULINA
 size = (width, height)
 screen = pygame.display.set_mode(size)
+pygame.init()
 
+font = pygame.font.SysFont("monospace", 30)
 
 def creareTabla():
     return np.zeros((RANDURI, COLOANE))
@@ -32,7 +34,7 @@ def urmRandLiber(tabla, coloana):
     for r in range(RANDURI):
         if tabla[r][coloana] == 0:
             return r
-    return None 
+    return None
 
 
 def puneBulina(tabla, rand, coloana, bulina):
@@ -90,10 +92,80 @@ def deseneazaTabla(tabla):
     pygame.display.update()
 
 
+def checkWin(tabla, piece):
+    for r in range(RANDURI):
+        for c in range(COLOANE - 3):
+            if (
+                tabla[r][c] == piece
+                and tabla[r][c + 1] == piece
+                and tabla[r][c + 2] == piece
+                and tabla[r][c + 3] == piece
+            ):
+                return True
+
+    for c in range(COLOANE):
+        for r in range(RANDURI - 3):
+            if (
+                tabla[r][c] == piece
+                and tabla[r + 1][c] == piece
+                and tabla[r + 2][c] == piece
+                and tabla[r + 3][c] == piece
+            ):
+                return True
+
+    for r in range(RANDURI - 3):
+        for c in range(COLOANE - 3):
+            if (
+                tabla[r][c] == piece
+                and tabla[r + 1][c + 1] == piece
+                and tabla[r + 2][c + 2] == piece
+                and tabla[r + 3][c + 3] == piece
+            ):
+                return True
+
+    for r in range(3, RANDURI):
+        for c in range(COLOANE - 3):
+            if (
+                tabla[r][c] == piece
+                and tabla[r - 1][c + 1] == piece
+                and tabla[r - 2][c + 2] == piece
+                and tabla[r - 3][c + 3] == piece
+            ):
+                return True
+
+    return False
+
+
+def showWinner(winner):
+    popup_width, popup_height = 400, 200
+    popup_x = (width - popup_width) // 2
+    popup_y = (height - popup_height) // 2
+
+    popup_surface = pygame.Surface((popup_width, popup_height))
+    popup_surface.fill((30, 30, 30))
+
+    pygame.draw.rect(popup_surface, (255, 255, 255), (0, 0, popup_width, popup_height), 5)
+
+    if winner == 1:
+        label = font.render("Player Wins!", True, BULINEJUCATOR)
+    else:
+        label = font.render("Computer Wins!", True, BULINECALCULATOR)
+
+    label_x = (popup_width - label.get_width()) // 2
+    label_y = (popup_height - label.get_height()) // 2
+    popup_surface.blit(label, (label_x, label_y))
+
+    screen.blit(popup_surface, (popup_x, popup_y))
+    pygame.display.update()
+    pygame.time.wait(3000)
+    sys.exit()
+
+
+
 def main():
     tabla = creareTabla()
-    turn = random.randint(0, 1) 
-    pygame.init()
+    turn = random.randint(0, 1)
+
     deseneazaTabla(tabla)
 
     game_over = False
@@ -103,42 +175,47 @@ def main():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-            if event.type == pygame.MOUSEMOTION:
-                pygame.draw.rect(screen, FUNDAL, (0, 0, width, DIMENSIUNEBULINA))
-                posx = event.pos[0]
-                if turn == 0:
+            if turn == 0:  
+                if event.type == pygame.MOUSEMOTION:
+                    pygame.draw.rect(screen, FUNDAL, (0, 0, width, DIMENSIUNEBULINA))
+                    posx = event.pos[0]
                     pygame.draw.circle(
                         screen, BULINEJUCATOR, (posx, int(DIMENSIUNEBULINA / 2)), RAZA
                     )
+                pygame.display.update()
 
-            pygame.display.update()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pygame.draw.rect(screen, FUNDAL, (0, 0, width, DIMENSIUNEBULINA))
+                    posx = event.pos[0]
+                    coloana = int(math.floor(posx / DIMENSIUNEBULINA))
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                pygame.draw.rect(screen, FUNDAL, (0, 0, width, DIMENSIUNEBULINA))
+                    if validLocatie(tabla, coloana):
+                        rand = urmRandLiber(tabla, coloana)
+                        puneBulina(tabla, rand, coloana, 1)
 
+                        if checkWin(tabla, 1):
+                            game_over = True
+                            showWinner(1)
 
-                posx = event.pos[0]
-                coloana = int(math.floor(posx / DIMENSIUNEBULINA))
+                        turn = 1
+                        deseneazaTabla(tabla)
 
-                if validLocatie(tabla, coloana):
-                    rand = urmRandLiber(tabla, coloana)
-                    puneBulina(tabla, rand, coloana, 1)
+        if turn == 1 and not game_over:
+            pygame.time.wait(500)
+            coloana = random.randint(0, COLOANE - 1)
 
-                    turn = 1 
+            while not validLocatie(tabla, coloana):
+                coloana = random.randint(0, COLOANE - 1)
 
+            rand = urmRandLiber(tabla, coloana)
+            puneBulina(tabla, rand, coloana, 2)
 
-                    coloana = random.randint(0, COLOANE - 1)
+            if checkWin(tabla, 2):
+                game_over = True
+                showWinner(2)
 
-                    while not validLocatie(tabla, coloana):
-                        coloana = random.randint(0, COLOANE - 1)
-
-                pygame.time.wait(500)
-                rand = urmRandLiber(tabla, coloana)
-                puneBulina(tabla, rand, coloana, 2)
-
-                turn = 0 
-
-                deseneazaTabla(tabla)
+            turn = 0
+            deseneazaTabla(tabla)
 
 
 if __name__ == "__main__":
